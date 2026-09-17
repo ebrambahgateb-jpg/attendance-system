7dff334390050a136e58d28d9bf1e7092c227de3
ebrambahgateb-jpg <ebrambahgat.eb@gmail.com>
2026-09-17 03:58:02 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxiwcSISS4P33T0_lkxXVT4fIvsnjLus9qNCCV4SE0AeiNuuiXuTwuY7ls9LUktdqIzMw/exec',
-  GOOGLE_CLIENT_ID: '869477346331-hbdiou239jbo6ldu8q47u6jel6gb7019.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '869477346331-rfnc3p57paa5baqfujiid0gtuh7ttv8q.apps.googleusercontent.com',
   APP_NAME: 'نظام تسجيل الحضور'https://script.google.com/macros/s/AKfycbxiwcSISS4P33T0_lkxXVT4fIvsnjLus9qNCCV4SE0AeiNuuiXuTwuY7ls9LUktdqIzMw/exec
