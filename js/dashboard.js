cefa029196fd675fc11679ac9381d75a53e6ae89
ebrambahgateb-jpg <ebrambahgat.eb@gmail.com>
2026-09-19 12:04:58 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -620,10 +620,6 @@ function loadMyAttendanceLazy(area) {
   if (typeof window.loadMyAttendancePage === 'function') window.loadMyAttendancePage(area);
   else showLoadError(area, 'سجل حضورك بنفسك');
 }
-function loadMyAttendanceLazy(area) {
-  if (typeof window.loadMyAttendancePage === 'function') window.loadMyAttendancePage(area);
-  else showLoadError(area, 'سجل حضورك بنفسك');
-}
 
 function showLoadError(area, name) {
   console.error(`❌ load${name}Page not found`);
